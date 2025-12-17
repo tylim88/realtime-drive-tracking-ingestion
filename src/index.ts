c232b15f78c5140b6ef96517f11b8fff55bf0f23
tylim <limteikyee@gmail.com>
2025-12-18 00:47:12 +0800

    setup elysia

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export * from '@/isOdd' // absolute path
-export * from './isEven' // relative path
+import { Elysia } from 'elysia'
+import { node } from '@elysiajs/node'
+
+new Elysia({ adapter: node() })
+	.post('/event', ({ body, status, set }) => {
+		console.log(Date.now())
+	})
+	.listen(3000)
