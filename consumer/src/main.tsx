01598837fc5f590461de4e86f7c1f273afb1fca6
tylim <limteikyee@gmail.com>
2025-12-25 14:19:35 +0800

    add notification

--- consumer/src/main.tsx
+++ consumer/src/main.tsx
@@ -2,6 +2,8 @@ import { MantineProvider } from '@mantine/core'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import { Notifications } from '@mantine/notifications'
 import App from './App.tsx'
 
 const root = document.getElementById('root')
@@ -10,6 +12,7 @@ if (root)
 	createRoot(root).render(
 		<StrictMode>
 			<MantineProvider>
+				<Notifications />
 				<App />
 			</MantineProvider>
 		</StrictMode>,
