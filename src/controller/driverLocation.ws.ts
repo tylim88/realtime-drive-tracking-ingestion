a67850bcf1a25b4de3663b79efc870f3db4fe935
tylim <limteikyee@gmail.com>
2025-12-25 14:19:23 +0800

    refactor driverLocation.ws

--- src/controller/driverLocation.ws.ts
+++ src/controller/driverLocation.ws.ts
@@ -7,77 +7,62 @@ import { db, driverLocations_schema } from '@/db'
 import { driverLocations_sub } from '@/pubsub'
 
 export const driverLocation_ws = () => {
-	// "current adapter doesn't support websocket" error
-	// https://github.com/elysiajs/elysia/issues/1008
-	const driverLocation_api = new Elysia({ adapter: node() }).ws(
-		'/driverLocation',
-		{
-			// validate query
-			query: t.Object({
-				driver_id: t.String(),
-				since: t.String({ format: 'date-time' }),
-				interval_type: t.Union([
-					t.Literal('second'),
-					t.Literal('minute'),
-					t.Literal('hour'),
-				]),
-				interval_value: t
-					.Transform(t.String())
-					.Decode((v) => {
-						if (Value.Check(Type.Integer({ minimum: 1 }), Number(v))) return v
-						throw new Error('interval_value must be an integer larger than 1')
-					})
-					.Encode((v) => v),
-			}),
-			// validate response
-			response,
-			// when websocket open
-			open: async (ws) => {
-				const { driver_id, since, interval_type, interval_value } =
-					ws.data.query
+	const driverLocation_api = new Elysia({
+		// "current adapter doesn't support websocket" error https://github.com/elysiajs/elysia/issues/1008
+		adapter: node(),
+	}).ws('/driverLocation', {
+		// when websocket open
+		open: async (ws) => {
+			const {
+				driver_id: driver_id_,
+				since,
+				interval_type,
+				interval_value,
+			} = ws.data.query
+			const driver_id = `driver_${driver_id_}`
+			if (!subscribers.has(driver_id)) subscribers.set(driver_id, new Set())
+			subscribers.get(driver_id)?.add(ws)
 
-				if (!subscribers.has(driver_id)) subscribers.set(driver_id, new Set())
-				subscribers.get(driver_id)?.add(ws)
-
-				const data = await db
-					// drizzle doesn't fully support timescale db
-					// have to type cast with generic
-					.select({
-						// https://www.tigerdata.com/docs/api/latest/hyperfunctions/time_bucket
-						time: sql<Date>`time_bucket(${interval_value} ${interval_type}, ${driverLocations_schema.recorded_at})`.as(
-							'time',
-						),
-						latitude_average: sql<number>`avg(${driverLocations_schema.latitude})`,
-						longitude_average: sql<number>`avg(${driverLocations_schema.longitude})`,
-					})
-					.from(driverLocations_schema)
-					.where(
-						and(
-							eq(driverLocations_schema.driver_id, driver_id),
-							gte(driverLocations_schema.recorded_at, new Date(since)),
-						),
-					)
-					.orderBy(driverLocations_schema.recorded_at)
-
-				ws.send({
-					type: 'data_old',
-					data: data.map(({ latitude_average, longitude_average, time }) => ({
-						latitude_average,
-						longitude_average,
-						time: time.toISOString(),
-					})),
+			const data = await db
+				// drizzle doesn't fully support timescale db, have to type cast with generic
+				.select({
+					// https://www.tigerdata.com/docs/api/latest/hyperfunctions/time_bucket
+					time: sql<Date>`time_bucket(${interval_value} ${interval_type}, ${driverLocations_schema.recorded_at})`.as(
+						'time',
+					),
+					latitude_average: sql<number>`avg(${driverLocations_schema.latitude})`,
+					longitude_average: sql<number>`avg(${driverLocations_schema.longitude})`,
 				})
-			},
-			// when websocket close
-			close: (ws) => {
-				const { driver_id } = ws.data.query
-				subscribers.get(driver_id)?.delete(ws)
-				if (!subscribers.get(driver_id)?.size) subscribers.delete(driver_id)
-			},
+				.from(driverLocations_schema)
+				.where(
+					and(
+						eq(driverLocations_schema.driver_id, driver_id),
+						gte(driverLocations_schema.recorded_at, new Date(since)),
+					),
+				)
+				.orderBy(driverLocations_schema.recorded_at)
+
+			ws.send({
+				type: 'data_old',
+				data: data.map(({ latitude_average, longitude_average, time }) => ({
+					latitude_average,
+					longitude_average,
+					time: time.toISOString(),
+				})),
+			})
 		},
-	)
-	// use map over object because we going to change the key and value often
-	// https://stackoverflow.com/a/37994079/5338829
+		// validate query
+		query,
+		// validate response
+		response,
+		// when websocket close
+		close: (ws) => {
+			const { driver_id } = ws.data.query
+			subscribers.get(driver_id)?.delete(ws)
+			if (!subscribers.get(driver_id)?.size) subscribers.delete(driver_id)
+		},
+	})
+	// use map over object because we going to change the key and value often https://stackoverflow.com/a/37994079/5338829
 	// use Set to make sure the ws object stored is unique
 	const subscribers = new Map<
 		string,
@@ -121,3 +106,24 @@ const response = t.Union([
 		longitude: t.Number(),
 	}),
 ])
+
+const positiveStringInteger = t
+	.Transform(t.String())
+	.Decode((v) => {
+		if (Value.Check(Type.Integer({ minimum: 1 }), Number(v))) return v
+		throw new Error('value must be an integer larger than 1')
+	})
+	.Encode((v) => v)
+
+const query = t.Object({
+	driver_id: positiveStringInteger,
+	since: t.String({ format: 'date-time' }),
+	interval_type: t.Union([
+		t.Literal('second'),
+		t.Literal('minute'),
+		t.Literal('hour'),
+	]),
+	interval_value: positiveStringInteger,
+})
+
+export type Query = Static<typeof query>
